14ca75c458354545d7b533590608fe504fa24fb0
AlexandreMBo <alexandre.bordignon@uel.br>
2026-09-22 02:33:39 -0300

    Adicionada estrutura entidade Item.java

---
+++ src/main/java/lista_de_compras/model/Item.java
@@ -0,0 +1,17 @@
+//Definição da Entidade
+package lista_de_compras.model;
+
+//Mapeamento JPA - Montagem do Banco de Dados
+public class Item {
+
+//Atributos - Campos da Tabela
+    //Definição da estrutura de dados dos produtos
+
+
+// Construtores
+    //Instanciar e Inicializar os objetos do tipo Item
+
+
+//Getters e Setters
+    //Encapsulamento - Acesso seguro aos dados
+}
