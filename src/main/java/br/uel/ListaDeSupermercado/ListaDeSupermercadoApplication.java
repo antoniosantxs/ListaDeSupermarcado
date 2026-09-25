2d2319be3bd681cf2b2f0d2b38a3d70467f3a9a6
antoniosantxs <antoniosantos.x@hotmail.com>
2026-09-25 12:47:55 -0300

    Spring Initializr - ListaDeSupermercado (Uploud)

---
+++ src/main/java/br/uel/ListaDeSupermercado/ListaDeSupermercadoApplication.java
@@ -0,0 +1,13 @@
+package br.uel.ListaDeSupermercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaDeSupermercadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaDeSupermercadoApplication.class, args);
+	}
+
+}
